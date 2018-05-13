eb577ca04821a89e2ee29903ca8b91de93a9665b
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2018-05-13 21:07:56 +0300

    hello update

--- sample/hello.c
+++ sample/hello.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 int func(int a, int b, int c, int d, int e, int f, int g) {
-  return a + b - c + d - e + f - g;
+  return a + b + c + d + e + f + g;
 }
 
 int main() {
   for (int i = 0; i < 10; ++i) {
-    int a = 0;
+    int a = i;
     int b = 1;
-    int c = i;
-    int d = i + 1; 
-    int e = c;
-    int f = e + 1;
-    int g = i * i;
+    int c = 2;
+    int d = 3;
+    int e = 0;
+    int f = 0;
+    int g = 0;
     int ret = func(a, b, c, d, e, f, g);
-    fprintf(stderr, "func(...) return : %d \n", ret);
+    fprintf(stdout, "func(...) return : %d \n", ret);
   }
-  fprintf(stderr, "Hello World !!! \n");
+  fprintf(stdout, "Hello World !!! \n");
   return 0;
 }
 
