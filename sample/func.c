68655811e52c31f95df82fb63babe3382e413d14
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2018-05-31 20:36:00 +0300

    added func.so

---
+++ sample/func.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int sum2(int a, int b) {
+  fprintf(stdout, "sum2 \n");
+  return a + b;
+}
+
+int sum3(int a, int b) {
+  fprintf(stdout, "sum3 \n");
+  return a + b;
+}
+
---
+++ sample/func.h
@@ -0,0 +1,9 @@
+
+#ifndef __func_h__
+#define __func_h__
+
+int sum2(int a, int b);
+int sum3(int a, int b);
+
+#endif  // __func_h__
+
