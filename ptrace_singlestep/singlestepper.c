14b718faf87e7ab0367129ac4614fd31e2efcbf8
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2018-06-02 22:32:37 +0300

    getting absolute pointer via proc_maps_t

--- ptrace_singlestep/singlestepper.c
+++ ptrace_singlestep/singlestepper.c
@@ -11,9 +11,10 @@
 #include <dlfcn.h>
 #include "../elf/elf_utils.h"
 
+#define TRACE(a...) { fprintf(stderr, "TRACE [%d, %s, %d] ", getpid(), __FUNCTION__, __LINE__); fprintf(stderr, a); fflush(stderr); }
+#define ERROR(a...) { fprintf(stderr, "ERROR [%d, %s, %d] ", getpid(), __FUNCTION__, __LINE__); fprintf(stderr, a); fflush(stderr); }
+#define DEBUG(a...) { fprintf(stderr, "DEBUG [%d, %s, %d] ", getpid(), __FUNCTION__, __LINE__); fprintf(stderr, a); fflush(stderr); }
 #define DEBUG_STEPPER(a...) // { fprintf(stderr, "[%s, %d] stepper: ", __FUNCTION__, __LINE__); fprintf(stderr, a); fflush(stderr); }
-#define DEBUG(a...) { fprintf(stderr, "DEBUG [%s, %d] ", __FUNCTION__, __LINE__); fprintf(stderr, a); fflush(stderr); }
-#define TRACE(a...) { fprintf(stderr, "TRACE [%s, %d] ", __FUNCTION__, __LINE__); fprintf(stderr, a); fflush(stderr); }
 
 #define TIMEOUT_CHECK_LOCK_FILE   10
 #define LOCK_FILE_NAME            "/tmp/SINGLE_STEPPER_LOCK"
@@ -26,7 +27,7 @@ volatile int break_flag = 0;
 
 
 void signal_handler(int sig) {
-  DEBUG("sig %d pid %d \n", sig, getpid());
+  DEBUG("sig %d \n", sig);
   switch (sig) {
     case SIGINT:
     case SIGUSR1: {
@@ -40,16 +41,38 @@ void signal_handler(int sig) {
 
 typedef void (*callback_t)(struct user_regs_struct*);
 
-struct func_call_t {
+struct func_call_t { // old
   const char* callbacks_shared;   // Имя динамической библиотеки, содержащее новую функцию.
   const char* func_name;          // Имя перехватыемой функции.
   Elf64_Addr func_addr;           // Адрес перехватываемой функции.
-  unsigned long long rbp_call;    // Для определения сответствующей RET инструкции.
+  unsigned long long rbp_call;    // Для определения сответствующей RET инструкции. // TODO rip.
+  unsigned long long int ip;      // instruction_pointer
   const char* func_call_name;     // Имя новой функции для CALL инструкции.
   const char* func_ret_name;      // Имя новой функции для RET инструкции.
   callback_t callback_call;       // Указатель на новую функцию с именем func_call_name.
   callback_t callback_ret;        // Указатель на новую функцию с именем func_ret_name.
 };
+
+
+
+struct ic_t;
+
+int ic_init(struct ic_t* context, const char* shn, const char* fn);
+int ic_destroy(struct ic_t* context);
+int ic_debug(struct ic_t* context);
+
+
+
+struct pm_t { // proc_maps_t
+  pid_t pid;
+  FILE* maps;
+};
+
+int pm_init(struct pm_t* context, pid_t pid);
+int pm_get_ip(struct pm_t* context, const char* fn, long long int* ip);
+int pm_destroy(struct pm_t* context);
+
+
 
 struct user_info_t {
   struct user_regs_struct regs;
@@ -73,40 +96,32 @@ void print_info(FILE *stream, struct user_info_t* info) {
   fprintf(stream, " \n\n");
 }
 
-void fprint_wait_status(FILE *stream, int status) {
-  if (WIFSTOPPED(status)) {
-    fprintf(stream, "Child stopped: %d\n", WSTOPSIG(status));
-  }
-  if (WIFEXITED(status)) {
-    fprintf(stream, "Child exited: %d\n", WEXITSTATUS(status));
-  }
-  if (WIFSIGNALED(status)) {
-    fprintf(stream, "Child signaled: %d\n", WTERMSIG(status));
-  }
-  if (WCOREDUMP(status)) {
-    fprintf(stream, "Core dumped.\n");
-  }
-}
-
 int ptrace_instruction_pointer(int pid, struct user_info_t *info) {
   if (!info)
     return -1;
 
   if (ptrace(PTRACE_GETREGS, pid, NULL, &info->regs)) {
-    fprintf(stderr, "Error fetching registers from child process: %s\n", strerror(errno));
+    ERROR("  Error fetching registers from child process: %s\n", strerror(errno));
     return -1;
   }
 
   info->last_error = 0;
 
   long ins = ptrace(PTRACE_PEEKTEXT, pid, info->regs.rip, NULL);
-  long ins_copy = ins;
+  // long ins_copy = ins;
 
-  unsigned char opcode = ins & 0xFF;
-  ins_copy >>= 8;
+  // unsigned char opcode = ins & 0xFF;
+  // ins_copy >>= 8;
 
-  DEBUG_STEPPER("INS:  %16lx \n", ins);
+  DEBUG_STEPPER("INS:  %16lx   %16llx \n", ins, info->regs.rip);
 
+  if (info->regs.rip == info->func_call.ip) {
+    if (info->func_call.callback_call) {
+      ((callback_t) info->func_call.callback_call)(&info->regs);
+    }
+  }
+
+  /*
   if (opcode == 0xE8) {
     int ptr_offset = ins_copy & 0xFFFFFFFF;
     long ptr = info->regs.rip + ptr_offset + sizeof(opcode) + sizeof(ptr_offset);
@@ -117,6 +132,8 @@ int ptrace_instruction_pointer(int pid, struct user_info_t *info) {
     DEBUG_STEPPER("arg4 rcx: %llx \n", info->regs.rcx);
     DEBUG_STEPPER("arg5  r8: %llx \n", info->regs.r8);
     DEBUG_STEPPER("arg6  r9: %llx \n", info->regs.r9);
+    DEBUG_STEPPER("rbp: %llx \n", info->regs.rbp);
+    DEBUG_STEPPER("rsp: %llx \n", info->regs.rsp);
 
     if ((ptr & 0xFFF) == info->func_call.func_addr) { // TODO
       printf("  %hhx CALL   ptr: %x %lx \n", opcode, ptr_offset, ptr);
@@ -126,19 +143,19 @@ int ptrace_instruction_pointer(int pid, struct user_info_t *info) {
           ((callback_t) info->func_call.callback_call)(&info->regs);
         }
       } else {
-        fprintf(stderr, "  ERROR: nested call \n");
+        ERROR("  ERROR: nested call \n");
       }
     }
 
-    /*
     if ((ptr & 0xFFF) == info->addr1) { // HACK
       info->regs.rdi = -info->regs.rdi;
       ptrace(PTRACE_SETREGS, pid, NULL, &info->regs);
     }
-    */
   } else if (opcode == 0xC3) {
     DEBUG_STEPPER("  %hhx RET \n", opcode);
     DEBUG_STEPPER("ret rax: %llx \n", info->regs.rax);
+    DEBUG_STEPPER("rbp: %llx \n", info->regs.rbp);
+    DEBUG_STEPPER("rsp: %llx \n", info->regs.rsp);
     if (info->func_call.rbp_call && info->func_call.rbp_call == info->regs.rbp) {
       info->func_call.rbp_call = 0;
       if (info->func_call.callback_ret) {
@@ -149,6 +166,7 @@ int ptrace_instruction_pointer(int pid, struct user_info_t *info) {
     // DEBUG_STEPPER("opcр: %16hhx \n", opcode);
     info->last_error = -1;
   }
+  */
 
   // print_info(stderr, info);
 
@@ -172,6 +190,9 @@ struct context_symbols_t {
 };
 
 int context_symbols_init(struct context_symbols_t* context, const char* callbacks_shared) {
+  context->fd = 0;
+  context->st_size = 0;
+  context->elf = NULL;
   return open_elf(callbacks_shared, &context->fd, &context->st_size, &context->elf);
 }
 
@@ -181,6 +202,9 @@ int context_symbols_lookup(struct context_symbols_t* context, const char* funcna
 }
 
 int context_symbols_destroy(struct context_symbols_t* context) {
+  context->fd = 0;
+  context->st_size = 0;
+  context->elf = NULL;
   return close_elf(&context->fd, &context->st_size, &context->elf);
 }
 
@@ -191,17 +215,23 @@ struct context_dl_t {
 };
 
 int context_dl_init(struct context_dl_t* context, const char* callbacks_shared) {
+  TRACE(" \n");
   context->handle = dlopen(callbacks_shared, RTLD_NOW);
+  TRACE("~ \n");
   return (!context->handle);
 }
 
 int context_dl_sym(struct context_dl_t* context, const char* funcname, void** callback) {
+  TRACE(" \n");
   void* fn = dlsym(context->handle, funcname);
   *callback = fn;
+  TRACE("~ \n");
   return dlerror() != NULL;
 }
 
 int context_dl_destroy(struct context_dl_t* context) {
+  TRACE(" \n");
+  TRACE("~ \n");
   return dlclose(context->handle);
 }
 
@@ -222,7 +252,7 @@ int process_add_children(struct process_t* context, pid_t pid);
 int process_destroy(struct process_t* context);
 
 int process_init(struct process_t* context, pid_t pid, struct func_call_t func_call) {
-  TRACE("pid: %d \n", pid);
+  TRACE(" \n");
   context->pid = pid;
   memset(&context->children, 0x00, sizeof(context->children));
   context->user_info.func_call = func_call;
@@ -232,12 +262,13 @@ int process_init(struct process_t* context, pid_t pid, struct func_call_t func_c
   waitpid(context->pid, &context->status, 0);
   ptrace(PTRACE_SETOPTIONS, context->pid, NULL, PTRACE_O_TRACEFORK);
   // fprint_wait_status(stderr, context->status);
-  TRACE("~ pid: %d \n", pid);
+  TRACE("~ \n");
   return 0;
 }
 
 int process_run(struct process_t* context) {
-  TRACE("pid: %d \n", context->pid);
+  TRACE(" \n");
+  DEBUG("pid: %d \n", context->pid);
   while (WIFSTOPPED(context->status) && !break_flag) {
     process_check_status(context);
     if (ptrace_instruction_pointer(context->pid, &context->user_info)) {
@@ -245,12 +276,13 @@ int process_run(struct process_t* context) {
     }
     context->status = singlestep(context->pid);
   }
-  TRACE("~ pid: %d \n", context->pid);
+  TRACE("~ \n");
   return 0;
 }
 
 int process_wait_children(struct process_t* context) {
-  TRACE("pid: %d \n", context->pid);
+  TRACE(" \n");
+  DEBUG("pid: %d \n", context->pid);
   for (int i = 0; i < PROCESS_MAX_COUNT; ++i) {
     if (context->children[i]) {
       int status;
@@ -259,7 +291,7 @@ int process_wait_children(struct process_t* context) {
       waitpid(context->children[i], &status, 0);
     }
   }
-  TRACE("~ pid: %d \n", context->pid);
+  TRACE("~ \n");
   return 0;
 }
 
@@ -309,12 +341,136 @@ int process_add_children(struct process_t* context, pid_t pid) {
 }
 
 int process_destroy(struct process_t* context) {
-  TRACE("pid: %d \n", context->pid);
+  TRACE(" \n");
+  DEBUG("pid: %d \n", context->pid);
   ptrace(PTRACE_DETACH, context->pid, NULL, NULL);
   context->pid = 0;
   memset(&context->children, 0x00, sizeof(context->children));
   context->status = 0;
-  TRACE("~ pid: %d \n", context->pid);
+  TRACE("~ \n");
+  return 0;
+}
+
+
+
+// IMPLEMENTATION
+
+
+
+struct ic_t { // instruction_callback_t
+  long long int ip;    // instruction_pointer
+  const char*   shn;   // shared_name
+  const char*   fn;    // function_name
+  void*         cb;    // callback
+  struct context_dl_t dl;
+};
+
+int ic_init(struct ic_t* context, const char* shn, const char* fn) {
+  TRACE(" \n");
+  context->ip = 0;
+  context->shn = shn;
+  context->fn = fn;
+  context->cb = NULL;
+  context_dl_init(&context->dl, context->shn);
+  TRACE("~ \n");
+  return 0;
+}
+
+int ic_destroy(struct ic_t* context) {
+  TRACE(" \n");
+  context->ip = 0;
+  context->shn = NULL;
+  context->fn = NULL;
+  context->cb = NULL;
+  context_dl_destroy(&context->dl);
+  TRACE("~ \n");
+  return 0;
+}
+
+int ic_debug(struct ic_t* context){
+  TRACE(" \n");
+  DEBUG("ic: {ip: %16llx, shn: '%s', fn: '%s', cb: %p} \n",
+      context->ip, context->shn, context->fn, context->cb);
+  TRACE("~ \n");
+  return 0;
+}
+
+int ic_set_callback(struct ic_t* context) {
+  TRACE(" \n");
+  context_dl_sym(&context->dl, context->fn, &context->cb);
+  TRACE("~ \n");
+  return 0;
+}
+
+
+
+int pm_init(struct pm_t* context, pid_t pid) {
+  TRACE(" \n");
+  char maps_path[50];
+  snprintf(maps_path, 50, "/proc/%d/maps", pid);
+
+  context->maps = fopen(maps_path, "r");
+	if(!context->maps){
+		ERROR("  Cannot open the memory maps, %s\n", strerror(errno));
+		return -1;
+  }
+
+  TRACE("~ \n");
+  return 0;
+}
+
+int pm_get_ip(struct pm_t* context, const char* fn, long long int* ip) {
+  TRACE(" \n");
+
+  char* line = NULL;
+  size_t len = 0;
+
+  long long int addr1;
+  long long int addr2;
+  long long int offset;
+  char perm[5] = {};
+  char pathname[500] = {};
+
+  rewind(context->maps);
+
+  while ((getline(&line, &len, context->maps)) != -1) {
+    sscanf(line, "%llx-%llx %4s %llx %*s %*s %499s", &addr1, &addr2, perm, &offset, pathname);
+    // DEBUG("addr: %16llx %16llx %16llx '%s' '%s' \n", addr1, addr2, offset, perm, pathname);
+    if ('x' == perm[2] && '/' == pathname[0]) {
+      DEBUG("addr: %16llx %16llx %16llx '%s' '%s' \n", addr1, addr2, offset, perm, pathname);
+
+      {
+        struct context_symbols_t context_symbols;
+        context_symbols_init(&context_symbols, pathname);
+
+        Elf64_Addr addr = 0;
+        context_symbols_lookup(&context_symbols, fn, &addr);
+
+        context_symbols_destroy(&context_symbols);
+
+        if (addr) {
+          *ip = addr + addr1;
+          break;
+        }
+      }
+    }
+  }
+
+  // perm == "r-xp" => Use for store text code and const varibles.
+  // perm == "r--p" => Use for GNU_RELRO relocated info.
+  // perm == "rw-p" => Use for bss data segment.
+
+  TRACE("~ \n");
+  return 0;
+}
+
+int pm_destroy(struct pm_t* context) {
+  TRACE(" \n");
+
+  fclose(context->maps);
+  context->maps = NULL;
+
+  TRACE("~ \n");
   return 0;
 }
 
@@ -324,15 +480,17 @@ int main(int argc, char ** argv/*, char **envp*/) {
     struct user_info_t info;
 
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s elffile pid \n", argv[0]);
+        ERROR("  Usage: %s elffile pid \n", argv[0]);
         exit(-1);
     }
 
     signal(SIGUSR1, signal_handler);
     signal(SIGINT,  signal_handler);
 
+    // old
     info.func_call.callbacks_shared = "./sample/callbacks.so";
     info.func_call.func_name = "sum";
+    info.func_call.func_name = "func_10";
     info.func_call.func_addr = 0;
     info.func_call.rbp_call = 0;
     info.func_call.func_call_name = "sum_call";
@@ -340,30 +498,53 @@ int main(int argc, char ** argv/*, char **envp*/) {
     info.func_call.callback_call = NULL;
     info.func_call.callback_ret = NULL;
 
-    {
+    /*
+    { // old
       struct context_symbols_t context_symbols;
       context_symbols_init(&context_symbols, argv[1]);
 
       Elf64_Addr addr;
       context_symbols_lookup(&context_symbols, info.func_call.func_name, &addr);
-      fprintf(stderr, "func_name: %s   addr: %lx \n", info.func_call.func_name, addr);
+      DEBUG("func_name: %s   addr: %lx \n", info.func_call.func_name, addr);
       info.func_call.func_addr = addr;
 
       context_symbols_destroy(&context_symbols);
     }
 
-    {
+    { // old
       struct context_dl_t context_dl;
       context_dl_init(&context_dl, info.func_call.callbacks_shared);
 
       context_dl_sym(&context_dl, info.func_call.func_call_name, (void**) &info.func_call.callback_call);
-      fprintf(stderr, "func_call: %s   addr: %p \n", info.func_call.func_call_name,
+      DEBUG("func_call: %s   addr: %p \n", info.func_call.func_call_name,
               (void *) info.func_call.callback_call);
       context_dl_sym(&context_dl, info.func_call.func_ret_name, (void**) &info.func_call.callback_ret);
-      fprintf(stderr, "func_ret:  %s   addr: %p \n", info.func_call.func_ret_name,
+      DEBUG("func_ret:  %s   addr: %p \n", info.func_call.func_ret_name,
               (void *) info.func_call.callback_ret);
 
       // context_dl_destroy(&context_dl);
+    }
+    */
+
+
+
+    {
+      struct ic_t ic;
+      ic_init(&ic, "./sample/callbacks.so", "sum_call");
+
+      struct pm_t pm;
+      pm_init(&pm, atoi(argv[2]));
+      pm_get_ip(&pm, "func_10", &ic.ip);
+      pm_destroy(&pm);
+
+      ic_set_callback(&ic);
+
+      // TODO
+      info.func_call.ip = ic.ip;
+      info.func_call.callback_call = (callback_t) ic.cb;
+
+      ic_debug(&ic);
+      // ic_destroy(&ic);
     }
 
 
